213d87746a35473c73e3444cd70f385cb624f0c4
Adrian Stainforth <adrian@morsedigital.com>
2016-08-08 11:08:11 +0100

    fixing delete file

--- src/delete.js
+++ src/delete.js
@@ -1,27 +1,30 @@
-var _        = require('lodash')
-  , es   = require('event-stream')
-  , fs   = require('fs')
-  , read = require("./read");
+var _    = require('lodash');
+var es   = require('event-stream');
+var fs   = require('fs');
+var read = require('./read');
 
 exports.folder = function(folder, fileFilter){
   return function(cb, rmdir){
     var stream = read(folder, fileFilter);
     rmdir = _.isBoolean(rmdir) ? rmdir : false;
     stream
-      .on("end", function(d){
-        if(_.isFunction(cb)) cb();
-        if(rmdir) fs.rmdirSync(folder)
+      .on('end', function(){
+        if (_.isFunction(cb)) cb();
+        if (rmdir) fs.rmdirSync(folder);
       })
       .pipe(
-        es.mapSync(function (entry) {
+        es.mapSync(function(entry){
           fs.unlinkSync(entry.fullPath);
           return entry.fullPath;
         })
       );
-  }
-}
-
+  };
+};
 
 exports.file = function(path){
-  fs.unlinkSync(path);
-}
+  fs.access(path, fs.constants.F_OK, function(e){
+    if(!e){
+      fs.unlinkSync(path);
+    }
+  });
+};
--- src/watcher.js
+++ src/watcher.js
@@ -1,41 +1,40 @@
 var _           = require('lodash')
-  , chokidar    = require("chokidar")
-
+  , chokidar    = require('chokidar');
 
 var getName =  function(path){
-  return _.last(path.split("/"));
-}
+  return _.last(path.split('/'));
+};
 
 module.exports = function(input){
   console.log('watching', input);
   var watcher = chokidar.watch(input, {
     ignored: /[\/\\]\./
-  })
-  watcher.on('error', function(error) {
+  });
+  watcher.on('error', function(error){
     console.log('Error happened: %j', error);
   });
 
-  obj = {
-    addChange:function(fn){
-      if(!_.isFunction(fn)) return obj;
-      watcher.on('change', function(path, stats) {
+  var obj = {
+    addChange: function(fn){
+      if (!_.isFunction(fn)) return obj;
+      watcher.on('change', function(path){
         console.log('File %j has been changed', getName(path));
-        fn(getName(path), path)
+        fn(getName(path), path);
       });
 
       return obj;
     }
-    , onAdd:function(fn){
-      if(!_.isFunction(fn)) return obj;
+    , onAdd: function(fn){
+      if (!_.isFunction(fn)) return obj;
 
-      watcher.on('add', function(path) {
+      watcher.on('add', function(path){
         console.log('File %j has been added to watcher', getName(path));
         fn(getName(path), path);
       });
 
       return obj;
     }
-  }
+  };
 
   return obj;
-}
+};
